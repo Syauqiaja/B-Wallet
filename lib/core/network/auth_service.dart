5d1c5f2293d2a8609bf712a0e5c850a470e8acac
Ahmad Fajruddin Syauqi <64742395+Syauqiaja@users.noreply.github.com>
2024-01-02 21:23:07 +0700

    feat: adding firebase auth service by email

---
+++ lib/core/network/auth_service.dart
@@ -0,0 +1,32 @@
+import "package:firebase_auth/firebase_auth.dart";
+
+class AuthService{
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
+  static Future<SignInSignUpResult> createUser(String email, String pass) async{
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: pass);
+      return SignInSignUpResult(user: result.user);
+    } catch (e) {
+      return SignInSignUpResult(message: e.toString());
+    }
+  }
+
+  static Future<SignInSignUpResult> signInWithEmail(String email, String pass) async{
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: pass);
+      return SignInSignUpResult(user: result.user);
+    } catch (e) {
+      return SignInSignUpResult(message: e.toString());
+    }
+  }
+
+  static void signOut(){
+    _auth.signOut();
+  }
+}
+
+class SignInSignUpResult{
+  final User? user;
+  final String? message;
+  SignInSignUpResult({this.user, this.message});
+}
